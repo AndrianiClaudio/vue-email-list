9e980e780fbda2fb325c248a2da09fb534151ddc
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-10 14:07:29 +0100

    Stampa di una lista con 10 email diverse.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,23 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        mails: [],
     },
     created () {
-        
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                // attraevrso API genera email
+                .then((response) => {
+                    // console.log(response.data.response);
+                    this.mails.push(response.data.response);
+                })
+                // errore
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+    },
+    methods: {
     },
 });
